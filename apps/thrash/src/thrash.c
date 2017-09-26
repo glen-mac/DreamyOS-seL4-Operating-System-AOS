3dc5f5afb333865c25a8bab8cef7eebfc8980d59
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-09-26 18:25:15 +1000

    Added the thrash app

---
+++ apps/thrash/src/thrash.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <sel4/sel4.h>
+#include <sos.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ttyout.h>
+#include <unistd.h>
+#include <utils/page.h>
+
+int
+main(void)
+{
+    size_t npages = 100;
+    int buffer[npages * MAX_IO_BUF];
+
+    while(1) {
+        for (int i = 0; i < npages; i++) {
+            for (int j = 0; j < MAX_IO_BUF; ++j)
+                buffer[(i * MAX_IO_BUF) + j] = (i * MAX_IO_BUF) + j;
+        }
+
+        for (int i = 0; i < npages; i++) {
+            // Just to test everything is being copied correctly
+            for (int j = 0; j < MAX_IO_BUF; ++j) {
+                assert(buffer[(i * MAX_IO_BUF) + j] == (i * MAX_IO_BUF) + j);
+            }
+        }
+    }
+
+    return 0;
+}
