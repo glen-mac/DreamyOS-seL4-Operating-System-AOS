47323632e876482fb77efc2c5ee78f273342da48
Glenn McGuire <glennmcguire9@gmail.com>
2017-10-10 14:25:29 +1100

    passes all memory tests

--- apps/sos/src/syscall/sys_vm.c
+++ apps/sos/src/syscall/sys_vm.c
@@ -12,26 +12,30 @@
 int
 syscall_brk(proc *curproc)
 {
-    seL4_Word newbrk = seL4_GetMR(1);
+    intptr_t newbrk = seL4_GetMR(1);
     LOG_INFO("syscall: thread made sos_brk(%d)", newbrk);
 
-    seL4_Word *heap_b = &(curproc->p_addrspace->region_heap->start);
-    seL4_Word *heap_t = &(curproc->p_addrspace->region_heap->end);
+    intptr_t *heap_s = &(curproc->p_addrspace->region_heap->start);
+    intptr_t *heap_e = &(curproc->p_addrspace->region_heap->end);
+    intptr_t stack_s = curproc->p_addrspace->region_stack->start;
 
     /* Set return value as okay by default */
     seL4_SetMR(0, 0);
 
     /* If we actually desire to change heap brk */
     if (newbrk) {
-        if (*heap_b > newbrk) {
+        if (newbrk < *heap_s) {
             /* If the newbrk is silly, then we change return value */
+            seL4_SetMR(0, 1);
+        } else if (newbrk >= stack_s){
+            /* If the newbrk is actually WITHIN the DEFINED stack region */
             seL4_SetMR(0, 1);
         } else {
             /* otherwise we change the brk */
-            *heap_t = newbrk;
+            *heap_e = newbrk;
         }
     }
 
-    seL4_SetMR(1, *heap_t);
+    seL4_SetMR(1, *heap_e);
     return 2;
 }
