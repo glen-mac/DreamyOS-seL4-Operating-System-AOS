1fd844876061b45949700ed1db4991faf965af00
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-10-19 00:11:13 +1100

    Cleaned up event.*

--- apps/sos/src/event.c
+++ apps/sos/src/event.c
@@ -6,16 +6,16 @@
 
 #include "event.h"
 
-#include <vm/vm.h>
 #include <clock/clock.h>
-#include <cspace/cspace.h>
-#include <sys/debug.h>
-#include <sys/panic.h>
 #include <syscall/syscall.h>
 #include <proc/proc.h>
 #include <utils/util.h>
 #include "network.h"
 
+/* Return the proc ID from the badge upper bits */
+#define GET_PROCID_BADGE(x) (x >> 21)
+
+/* Private functions */
 static void start_first_proc(void);
 static void reap_dead_orphans(proc *init);
 
@@ -27,20 +27,19 @@ event_loop(const seL4_CPtr ep)
     seL4_MessageInfo_t message;
 
     /* Start the startup user application */
-    resume(coroutine(start_first_proc), NULL);
-
-    LOG_INFO("returned from start_first_proc");
+    resume(coroutine((void * (*)(void *))start_first_proc), NULL);
 
     proc *init = get_proc(0);
     assert(init != NULL);
 
     while (TRUE) {
+        /* Cleanup any of SOS' children */
         reap_dead_orphans(init);
 
         message = seL4_Wait(ep, &badge);
         label = seL4_MessageInfo_get_label(message);
+        /* Interrupt */
         if (badge & IRQ_EP_BADGE) {
-            /* Interrupt */
             if (badge & IRQ_BADGE_NETWORK)
                 network_irq();
 
@@ -49,9 +48,11 @@ event_loop(const seL4_CPtr ep)
                 timer_interrupt();
 
         } else if (label == seL4_VMFault) {
-            resume(coroutine(vm_fault), GET_PROCID_BADGE(badge));
+            /* VM Fault */
+            resume(coroutine((void * (*)(void *))vm_fault), (void *)GET_PROCID_BADGE(badge));
         } else if (label == seL4_NoFault) {
-            resume(coroutine(handle_syscall), GET_PROCID_BADGE(badge));
+            /* Handle syscall */
+            resume(coroutine((void * (*)(void *))handle_syscall), (void *)GET_PROCID_BADGE(badge));
         } else {
             LOG_INFO("Rootserver got an unknown message");
         }
@@ -65,8 +66,9 @@ event_loop(const seL4_CPtr ep)
 static void 
 reap_dead_orphans(proc *init)
 {
+    /* Search for a zombie child */
     for (struct list_node *curr = init->children->head; curr != NULL; curr = curr->next) {
-        proc *child = get_proc(curr->data);
+        proc *child = get_proc((pid_t)curr->data);
         assert(child != NULL);
         if (child->p_state == ZOMBIE) {
             proc_destroy(child);
@@ -74,7 +76,7 @@ reap_dead_orphans(proc *init)
              * We break because we dont want to keep iterating
              * as proc_destroy modifies the list, so if we keep iterating
              * we get invalid data.
-             * Breaking is fine, we will cleanup other children later
+             * Breaking is fine, we will cleanup other children the next time this is called
              */
             break;
         }
@@ -83,7 +85,9 @@ reap_dead_orphans(proc *init)
 
 /*
  * Start the first process 
- * Needs to be here because of NFS read
+ * Needs to be here because the elf file is read from NFS
+ * Also use this function because the proc_start we want to call takes multiple arguments
+ * which picoro doesnt support
  */
 static void
 start_first_proc(void)
